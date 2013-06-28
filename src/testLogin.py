96308faed37ed8b77c8aad271eec6bca521e7500
Ben Kang <bkang@2tor.com>
2013-06-28 14:07:58 -0400

    test

--- src/testLogin.py
+++ src/testLogin.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 
 driver = webdriver.Firefox()
 
+
 landing_page = pages.LandingPage(driver)
 landing_page.login('https://au-mir-oars-sb01-qa.2u.com')
 landing_page.teardown()
