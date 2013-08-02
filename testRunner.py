71c422ac557cfef907dcc897567688ecfce6f77a
Omar Griffin <ogriffin@2u.com>
2013-08-02 13:21:53 -0400

    removed an unnecessary command line argument

--- testRunner.py
+++ testRunner.py
@@ -10,14 +10,14 @@ parser = argparse.ArgumentParser()
 #input order does not matter
 parser.add_argument("--test", "-T",    action="store", type=str, required=True)
 parser.add_argument("--driver", "-D",     action="store", type=str, required=True)
-parser.add_argument("--program", "-P", action="store", type=str, required=True)
+#parser.add_argument("--program", "-P", action="store", type=str, required=True) #unnecessary as of now
 parser.add_argument("--url", "-U",     action="store", type=str, required=True)
 
 args = parser.parse_args()
 
 arg_map = {}
 arg_map['test'] = args.test #name of the python module containing the test you would like to run
-arg_map['program'] = args.program 
+#arg_map['program'] = args.program 
 arg_map['url'] = args.url #the url for the program
 arg_map['driver'] = args.driver	#store webdriver name
 arg_map['data'] = Filler(args.url) #instantiate and store Filler; used for inputting data on forms
