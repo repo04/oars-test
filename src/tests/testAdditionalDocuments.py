bc9a2f16b8ebfef5060bb44fb6d343e7fa9aef7c
Omar Griffin <ogriffin@2u.com>
2013-08-14 11:08:34 -0400

    test for additional docs page

---
+++ src/tests/testAdditionalDocuments.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from pages import LandingPage
+
+
+def test(args):
+  #setup
+  driver = args['driver']
+  program_url = args['url']
+  data = args['data']
+
+  landing_page = LandingPage(driver, 'Login Page', program_url)
+  landing_page.login(data)
+
+  #test
+  additional_documents = landing_page.navigate_to('Additional Documents')
+  data.auto_fill(additional_documents)
